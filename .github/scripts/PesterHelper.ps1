cd8e58aa83f285a0a2723842c19f5f7315e37a69
Marius Gerighausen <61243960+neo42mgn@users.noreply.github.com>
2023-07-27 17:15:57 +0200

    copy Testscript

--- .github/scripts/PesterHelper.ps1
+++ .github/scripts/PesterHelper.ps1
@@ -11,7 +11,8 @@ $dirname = "testrunfolder"
 New-Item -ItemType Directory -Name "$dirname" -Force
 Copy-Item "./PSAppDeployToolkit/Toolkit/*" "$dirname/" -Recurse -Force -Exclude $exclude
 Copy-Item "./PSAppDeployToolkitExtensions/*" -Recurse -Force -Destination "./$dirname/"
-Copy-Item "./test/*" -Destination "./$dirname/" -Recurse -Force
+Copy-Item "./PSAppDeployToolkitExtensions/test/shared.psm1" -Destination "./$dirname/" -Force
+Copy-Item "./PSAppDeployToolkitExtensions/test/PesterTests.ps1" -Destination "./$dirname/" -Force
 New-Item -ItemType Directory -Name "$dirname/SupportFiles/neo42-Userpart" -Force
 New-Item -ItemType File -Path "$dirname/SupportFiles/neo42-Userpart" -Name "place UserPart files here!!!"
 New-Item -ItemType Directory -Name "$dirname/Files" -Force
