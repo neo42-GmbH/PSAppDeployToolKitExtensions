612227d73993a26fab545fee43e93730f46c2ced
Marius Gerighausen <61243960+neo42mgn@users.noreply.github.com>
2023-08-16 09:54:21 +0200

    add 1 letter test

--- test/Compare-NxtVersion.Tests.ps1
+++ test/Compare-NxtVersion.Tests.ps1
@@ -2,42 +2,33 @@
 Describe "Compare-NxtVersion" {
     Context "With two equal version numbers" {
         It "Returns 1" {
-            # Arrange
             $detectedVersion = "01.0.0"
             $targetVersion = "1.0.0.0"
-
-            # Act
             $result = Compare-NxtVersion -DetectedVersion $detectedVersion -TargetVersion $targetVersion
-
-            # Assert
             $result | Should -Be 1
         }
     }
-
     Context "With a higher version number for TargetVersion" {
         It "Returns 2" {
-            # Arrange
             $detectedVersion = "1.0.0"
             $targetVersion = "2.0.0"
-
-            # Act
             $result = Compare-NxtVersion -DetectedVersion $detectedVersion -TargetVersion $targetVersion
-
-            # Assert
             $result | Should -Be 2
         }
     }
-
     Context "With a lower version number for TargetVersion" {
         It "Returns 3" {
-            # Arrange
             $detectedVersion = "2.0.0"
             $targetVersion = "1.0.0"
-
-            # Act
             $result = Compare-NxtVersion -DetectedVersion $detectedVersion -TargetVersion $targetVersion
-
-            # Assert
+            $result | Should -Be 3
+        }
+    }
+    Context "With a lower version number for TargetVersion" {
+        It "Returns 3" {
+            $detectedVersion = "b"
+            $targetVersion = "a"
+            $result = Compare-NxtVersion -DetectedVersion $detectedVersion -TargetVersion $targetVersion
             $result | Should -Be 3
         }
     }
