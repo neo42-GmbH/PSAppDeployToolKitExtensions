98f23d27f424d450f9707a6f44cb1a4c8238213c
Marius Gerighausen <61243960+neo42mgn@users.noreply.github.com>
2023-08-21 11:59:54 +0200

    Do not clean Userpart if userpartonuninstall true

--- AppDeployToolkit/Clean-Neo42AppFolder.ps1
+++ AppDeployToolkit/Clean-Neo42AppFolder.ps1
@@ -4,16 +4,22 @@
 .LINK
 	https://neo42.de/psappdeploytoolkit
 #>
-
 [CmdletBinding()]
 Param (
 )
 $currentScript = $MyInvocation.MyCommand.Definition
 try {
     Start-Sleep -Seconds 10
-    ## what is now "User" must be equal to $global:userPartDir in Deploy-Application.ps1
-    $folderToRemove = @("$PSScriptRoot\neo42-Install","$PSScriptRoot\neo42-Source","$PSScriptRoot\User" )
-
+    ## read neo42PackageConfig.json to determine if user uninstall is enabled
+    $packageConfig = Get-Content -Path "$PSScriptRoot\neo42-Install\neo42PackageConfig.json" | Out-String | ConvertFrom-Json
+    $folderToRemove = @("$PSScriptRoot\neo42-Install", "$PSScriptRoot\neo42-Source")
+    if ($true -eq $packageConfig.UserPartOnUninstallation) {
+        ## due to an active user part on UnInstallation, we must keep the userpart folder.
+    }
+    else {
+        ## what is now "User" must be equal to $global:userPartDir in Deploy-Application.ps1
+        $folderToRemove += "$PSScriptRoot\User"
+    }
     foreach ($folder in $folderToRemove) {
         if (Test-Path -Path $folder) {
             Remove-Item -Path $folder -Recurse -Force
