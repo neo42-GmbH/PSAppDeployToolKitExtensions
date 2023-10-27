e1ccd841b633f051cee6faec19c3addcd1e4a89f
Marius Gerighausen <61243960+neo42mgn@users.noreply.github.com>
2023-10-27 15:02:21 +0200

    Add Tests for Tempfolder Creation

---
+++ test/Definitions/New-NxtTemporaryFolder.Tests.ps1
@@ -0,0 +1,29 @@
+# Assuming the function New-NxtTemporaryFolder is loaded in the current session or sourced from another file.
+
+Describe "New-NxtTemporaryFolder" {
+    Context "Default TempRootPath" {
+        It "Uses default TempRootPath when none is provided" {
+            $tempFolder = New-NxtTemporaryFolder
+            $tempFolder | Should -BeLike "$env:SystemDrive\`$neo42tmp*"
+        }
+    }
+
+    Context "Folder Creation" {
+        It "Creates a folder in the specified TempRootPath" {
+            $tempPath = "C:\CustomTempPath"
+            $tempFolder = New-NxtTemporaryFolder -TempRootPath $tempPath
+            $tempFolder | Should -BeLike "$tempPath*"
+            Remove-Item -Path $tempPath -Recurse -Force
+        }
+    }
+    Context "Random Folder Name Generation" {
+        It "Generates a unique folder name" {
+            Mock Test-Path { return $true } -ParameterFilter { $Path -like "*ABC*" } # Assuming ABC is the random name generated
+            Mock Test-Path { return $false } -ParameterFilter { $Path -notlike "*ABC*" }
+
+            $tempFolder = New-NxtTemporaryFolder
+            $tempFolder | Should -Not -BeLike "*ABC*"
+        }
+    }
+}
+
