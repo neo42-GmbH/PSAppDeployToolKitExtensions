7e4021838ab2ab42b0aed43fa1a8c9399682d8ed
neo42JBR <julian.behr@neo42.de>
2023-11-07 17:36:41 +0100

    Added complex password for requirements

--- test/Definitions/Add-NxtLocalUser.Tests.ps1
+++ test/Definitions/Add-NxtLocalUser.Tests.ps1
@@ -9,24 +9,24 @@ Describe 'Add-NxtLocalUser' {
             }
         }
         It 'Should create a new user account' -Skip {
-            Add-NxtLocalUser -Username 'TestUser' -Password 'TestPassword' | Should -Be $true
+            Add-NxtLocalUser -Username 'TestUser' -Password 'JX{C@Wb<PHV=H}Gx' | Should -Be $true
             Get-LocalUser -Name 'TestUser' | Should -Not -Be $null
         }
         It 'Should have the correct password' {
-            Add-NxtLocalUser -Username 'TestUser' -Password 'TestPassword'
+            Add-NxtLocalUser -Username 'TestUser' -Password 'JX{C@Wb<PHV=H}Gx'
             [System.DirectoryServices.AccountManagement.PrincipalContext]$accounts = New-Object System.DirectoryServices.AccountManagement.PrincipalContext('machine', $env:COMPUTERNAME)
-            $accounts.ValidateCredentials('TestUser', 'TestPassword') | Should -Be $true
+            $accounts.ValidateCredentials('TestUser', 'JX{C@Wb<PHV=H}Gx') | Should -Be $true
         }
         It 'User should have defined attributes' {
-            Add-NxtLocalUser -Username 'TestUser' -Password 'TestPassword' -Description 'Test Description' -SetPwdNeverExpires -FullName 'Test User'
+            Add-NxtLocalUser -Username 'TestUser' -Password 'JX{C@Wb<PHV=H}Gx' -Description 'Test Description' -SetPwdNeverExpires -FullName 'Test User'
             [Microsoft.PowerShell.Commands.LocalUser]$user = Get-LocalUser -Name 'TestUser'
             $user.FullName | Should -Be 'Test User'
             $user.Description | Should -Be 'Test Description'
             $user.PasswordExpires | Should -Be $null
         }
         It 'Should update parameters when user exists' -Skip {
-            Add-NxtLocalUser -Username 'TestUser' -Password 'TestPassword' -Description 'Test Description' -SetPwdNeverExpires -FullName 'Test User'
-            Add-NxtLocalUser -UserName 'TestUser' -Password 'TestPassword2' -Description 'Test Description2' -SetPwdExpired -FullName 'Test User2' | Should -Be $true
+            Add-NxtLocalUser -Username 'TestUser' -Password 'JX{C@Wb<PHV=H}Gx' -Description 'Test Description' -SetPwdNeverExpires -FullName 'Test User'
+            Add-NxtLocalUser -UserName 'TestUser' -Password 'JX{C@Wb<PHV=H}Gx2' -Description 'Test Description2' -SetPwdExpired -FullName 'Test User2' | Should -Be $true
             [Microsoft.PowerShell.Commands.LocalUser]$user = Get-LocalUser -Name 'TestUser'
             $user.FullName | Should -Be 'Test User2'
             $user.Description | Should -Be 'Test Description2'
@@ -34,7 +34,7 @@ Describe 'Add-NxtLocalUser' {
             $user.PasswordChangeableDate | Should -BeLessOrEqual (Get-Date)
 
             [System.DirectoryServices.AccountManagement.PrincipalContext]$accounts = New-Object System.DirectoryServices.AccountManagement.PrincipalContext('machine', $env:COMPUTERNAME)
-            $accounts.ValidateCredentials('TestUser', 'TestPassword2') | Should -Be $true
+            $accounts.ValidateCredentials('TestUser', 'JX{C@Wb<PHV=H}Gx2') | Should -Be $true
         }
     }
 }
