ec5331e607ecde4eb5977666d1f27e85c60ebe3e
neo42JBR <julian.behr@neo42.de>
2023-11-14 10:11:59 +0100

    Fix language issue

--- test/Definitions/Set-NxtFolderPermissions.Tests.ps1
+++ test/Definitions/Set-NxtFolderPermissions.Tests.ps1
@@ -2,6 +2,14 @@ Describe "Set-NxtFolderPermissions" {
     Context "When the function is called" {
         BeforeAll {
             [string]$folder = "$PSScriptRoot\TestFolder"
+            function Get-NameOfSid{
+                param(
+                    [Parameter(Mandatory=$true)]
+                    [System.Security.Principal.WellKnownSidType]$SidName
+                )
+                [System.Security.Principal.SecurityIdentifier]$Sid = (New-Object System.Security.Principal.SecurityIdentifier -ArgumentList ($SidName, $null))
+                return (New-Object System.Security.Principal.SecurityIdentifier($sid.value)).Translate([System.Security.Principal.NTAccount]).Value
+            }
         }
         BeforeEach {
             New-Item -Path $folder -ItemType Directory | Out-Null
@@ -12,17 +20,18 @@ Describe "Set-NxtFolderPermissions" {
         It "Should set the folder permissions for a single Sid correctly" {
             Set-NxtFolderPermissions -Path $folder -Owner 'WorldSid' -FullControlPermissions 'WorldSid' -BreakInheritance $true | Should -BeNullOrEmpty
             [System.Security.AccessControl.DirectorySecurity]$acl = Get-Acl -Path $folder
-            $acl.Access[0].IdentityReference.Value | Should -Be 'Everyone'
+            [string]$name = (New-Object System.Security.Principal.SecurityIdentifier('S-1-5-18')).Translate([System.Security.Principal.NTAccount]).Value
+            $acl.Access[0].IdentityReference.Value | Should -Be (Get-NameOfSid -SidName 'WorldSid')
             $acl.Access[0].FileSystemRights | Should -Be 'FullControl'
             $acl.Access.Count | Should -Be 1
-            $acl.Owner | Should -Be 'Everyone'
+            $acl.Owner | Should -Be (Get-NameOfSid -SidName 'WorldSid')
             $acl.AreAccessRulesProtected | Should -Be $true
         }
         It "Should set the folder permissions for a group of Sids correctly" {
             Set-NxtFolderPermissions -Path $folder -Owner 'WorldSid' -FullControlPermissions 'WorldSid','BuiltinGuestsSid' | Should -BeNullOrEmpty
             [System.Security.AccessControl.DirectorySecurity]$acl = Get-Acl -Path $folder
-            $acl.Access.IdentityReference.Value | Should -Contain 'Everyone'
-            $acl.Access.IdentityReference.Value | Should -Contain 'BUILTIN\Guests'
+            $acl.Access.IdentityReference.Value | Should -Contain (Get-NameOfSid -SidName 'WorldSid')
+            $acl.Access.IdentityReference.Value | Should -Contain (Get-NameOfSid -SidName 'BuiltinGuestsSid')
             $acl.Access.Count | Should -Be 2
             $acl.Owner | Should -Be 'Everyone'
         }
@@ -39,8 +48,8 @@ Describe "Set-NxtFolderPermissions" {
             $access.SetOwner($everyone)
             Set-NxtFolderPermissions -Path $folder -CustomDirectorySecurity $access | Should -BeNullOrEmpty
             [System.Security.AccessControl.DirectorySecurity]$acl = Get-Acl -Path $folder
-            $acl.Access[0].IdentityReference.Value | Should -Be 'Everyone'
-            $acl.Owner | Should -Be 'Everyone'
+            $acl.Access[0].IdentityReference.Value | Should -Be (Get-NameOfSid -SidName 'WorldSid')
+            $acl.Owner | Should -Be (Get-NameOfSid -SidName 'WorldSid')
         }
         It "Should fail if any of the Sid's are invalid" {
             { Set-NxtFolderPermissions -Path $folder -Owner 'WorldSid' -FullControlPermissions 'WorldSid' -WritePermissions 'InvalidSid' } | Should -Throw
