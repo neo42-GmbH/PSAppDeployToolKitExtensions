091fe88c3586a59b5c14f766b16b7be25b9f1d6b
neo42JBR <julian.behr@neo42.de>
2023-11-17 13:48:01 +0100

    Skip check user process

--- test/Definitions/Get-NxtIsSystemProcess.Tests.ps1
+++ test/Definitions/Get-NxtIsSystemProcess.Tests.ps1
@@ -3,30 +3,22 @@ Describe "Get-NxtIsSystemProcess" {
         BeforeAll {
             [int]$systemProcessId = 4
             [int]$lsassProcessId = (Get-Process -Name 'lsass' | Select-Object -First 1).Id
-
-            [securestring]$securePassword = ConvertTo-SecureString -String "sdouighASDG42!" -AsPlainText -Force
-            [pscredential]$credential = New-Object System.Management.Automation.PSCredential ('TestUser', $securePassword)
-            New-LocalUser -Name 'TestUser' -Password $securePassword
-
-            [System.Diagnostics.Process]$userProcess = Start-Process -FilePath "timeout" -ArgumentList ("/t 300") -Credential $credential -PassThru -WorkingDirectory "$env:windir"
         }
-        AfterAll {
-            if (Get-LocalUser -Name 'TestUser' -ErrorAction SilentlyContinue) {
-                Remove-LocalUser -Name 'TestUser'
-            }
-            $userProcess.Kill()
-        }
-        It "Should return true for lsass process" {
+        It "Should return true for lsass (System) process" {
             $result = Get-NxtIsSystemProcess -ProcessId $lsassProcessId
             $result | Should -BeOfType 'bool'
             $result | Should -Be $true
         }
-        It "Should return false on self" {
-            Get-NxtIsSystemProcess -ProcessId $userProcess.Id | Should -Be $false
-        }
         It "Should throw on access denied" -Skip {
             # Issue #628
             Get-NxtIsSystemProcess -ProcessId $systemProcessId | Should -Be $true
+        }
+        It "Should return true for user prorcess" -Skip {
+            "@
+                It is currently hard to reliably create a user process for this test.
+                On self-hosted runners this process runs as System. System cannot impersonate a user.
+                This test will be skipped until a reliable way to create a user process is found.
+            @"
         }
     }
 }
