08536167ea018a8158fa02969a7f231289a9a65d
neo42JBR <julian.behr@neo42.de>
2023-11-17 11:00:35 +0100

    Add test process as user

--- test/Definitions/Get-NxtIsSystemProcess.Tests.ps1
+++ test/Definitions/Get-NxtIsSystemProcess.Tests.ps1
@@ -1,21 +1,33 @@
 Describe "Get-NxtIsSystemProcess" {
     Context "When given a system process name" {
         BeforeAll {
-            [int]$systemProcess = 4
-            [int]$userProcess = (Get-Process -Name 'explorer' | Select-Object -First 1).Id
-            [int]$lsassProcess = (Get-Process -Name 'lsass' | Select-Object -First 1).Id
+            [int]$systemProcessId = 4
+            [int]$lsassProcessId = (Get-Process -Name 'lsass' | Select-Object -First 1).Id
+
+            [securestring]$securePassword = ConvertTo-SecureString -String "sdouighASDG42!" -AsPlainText -Force
+            New-LocalUser -Name 'TestUser' -Password $securePassword
+            [pscredential]$credObject = New-Object System.Management.Automation.PSCredential ($userName, $secStringPassword)
+            [System.Diagnostics.Process]$userProcess = Start-Process -FilePath "simple.exe" -Credential $credObject
+        }
+        AfterAll {
+            if (Get-LocalUser -Name 'TestUser' -ErrorAction SilentlyContinue) {
+                Remove-LocalUser -Name 'TestUser'
+            }
+            if (Get-Process $process.Id -ErrorAction SilentlyContinue){
+                $userProcess.Kill()
+            }
         }
         It "Should return true for lsass process" {
-            $result = Get-NxtIsSystemProcess -ProcessId $lsassProcess
+            $result = Get-NxtIsSystemProcess -ProcessId $lsassProcessId
             $result | Should -BeOfType 'bool'
             $result | Should -Be $true
         }
         It "Should return false on self" {
-            Get-NxtIsSystemProcess -ProcessId $selfProcess | Should -Be $false
+            Get-NxtIsSystemProcess -ProcessId $userProcess.Id | Should -Be $false
         }
         It "Should throw on access denied" -Skip {
             # Issue #628
-            Get-NxtIsSystemProcess -ProcessId $systemProcess | Should -Be $true
+            Get-NxtIsSystemProcess -ProcessId $systemProcessId | Should -Be $true
         }
     }
 }
