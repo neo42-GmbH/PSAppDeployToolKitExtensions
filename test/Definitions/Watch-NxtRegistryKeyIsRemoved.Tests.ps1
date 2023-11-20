e60428c6ce8bc1ecf1f29fe3b6df13c689bf42b2
neo42JBR <julian.behr@neo42.de>
2023-11-20 15:42:29 +0100

    Add -Force parameter to job removal

--- test/Definitions/Watch-NxtRegistryKeyIsRemoved.Tests.ps1
+++ test/Definitions/Watch-NxtRegistryKeyIsRemoved.Tests.ps1
@@ -35,7 +35,7 @@ Describe "Watch-NxtRegistryKeyIsRemoved" {
             $result | Should -BeOfType 'bool'
             $result | Should -Be $true
             [Math]::Floor(((Get-Date) - $start).TotalSeconds) | Should -BeLessOrEqual 9
-            $job | Remove-Job
+            $job | Remove-Job -Force
         }
     }
 }
