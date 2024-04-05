815c1bd06f9669229663c6623f8e14797c4c40af
neo42JBR <julian.behr@neo42.de>
2024-04-05 17:33:01 +0200

    Add capital letter detection for param variables

--- .vscode/neo42PSScriptAnalyzerRules/neo42PSScriptAnalyzerRules.psm1
+++ .vscode/neo42PSScriptAnalyzerRules/neo42PSScriptAnalyzerRules.psm1
@@ -15,13 +15,14 @@ function neo42PSUseCorrectTokenCapitalization {
 		[Parameter(Mandatory = $true)]
 		[ValidateNotNullOrEmpty()]
 		[System.Management.Automation.Language.Token[]]
-		$testToken
+		$TestToken
 	)
 	Begin {
 		[string[]]$keywordList = @('if', 'else', 'elseif', 'function', 'foreach', 'for', 'while', 'do', 'in', 'switch', 'try', 'catch', 'finally', 'return', 'break', 'continue', 'throw', 'exit', 'Process', 'Begin', 'End', 'Param')
 	}
 	Process {
-		foreach ($token in $testToken) {
+		$results = @()
+		foreach ($token in $TestToken) {
 			## Check if the token is a keyword and if it is already in the correct case
 			if ($false -eq $token.TokenFlags.HasFlag([System.Management.Automation.Language.TokenFlags]::Keyword)) {
 				continue
@@ -75,16 +76,16 @@ function neo42PSUseCorrectCmdtletCapitalization {
 		[Parameter(Mandatory = $true)]
 		[ValidateNotNullOrEmpty()]
 		[System.Management.Automation.Language.ScriptBlockAst]
-		$testAst
+		$TestAst
 	)
 	Begin {
 		[string[]]$cmdtletNames = (Get-Command -CommandType Cmdlet -Module * -ErrorAction SilentlyContinue).Name
 	}
 	Process {
-		[System.Management.Automation.Language.CommandAst[]]$commandAsts = $testAst.FindAll({
+		[System.Management.Automation.Language.CommandAst[]]$commandAsts = $TestAst.FindAll({
 			$args[0] -is [System.Management.Automation.Language.CommandAst]
 		}, $false)
-
+		$results = @()
 		foreach ($commandAst in $commandAsts) {
 			if ($commandAst.InvocationOperator -ne 'Unknown') {
 				continue
@@ -115,6 +116,51 @@ function neo42PSUseCorrectCmdtletCapitalization {
 				'RuleName'             = $PSCmdlet.MyInvocation.InvocationName
 				'Severity'             = 'Warning'
 				'SuggestedCorrections' = $suggestedCorrections
+			}
+		}
+		return $results
+	}
+}
+
+function neo42PSVariablesFromParamBlockShouldBeCapitalized {
+	<#
+	.SYNOPSIS
+	Checks that parameter variables are capitalized.
+	.DESCRIPTION
+	Checks that parameter variables are capitalized.
+	.INPUTS
+	[System.Management.Automation.Language.ScriptBlockAst]
+	.OUTPUTS
+	[Microsoft.Windows.Powershell.ScriptAnalyzer.Generic.DiagnosticRecord[]]
+	#>
+	[CmdletBinding()]
+	[OutputType([Microsoft.Windows.PowerShell.ScriptAnalyzer.Generic.DiagnosticRecord[]])]
+	Param (
+		[Parameter(Mandatory = $true)]
+		[ValidateNotNullOrEmpty()]
+		[System.Management.Automation.Language.ScriptBlockAst]
+		$TestAst
+	)
+	Process {
+		[System.Management.Automation.Language.FunctionDefinitionAst[]]$functions = $TestAst.FindAll({ $args[0] -is [System.Management.Automation.Language.FunctionDefinitionAst] }, $false)
+		$results = @()
+		foreach ($functionAst in $functions) {
+			[System.Management.Automation.Language.ParamBlockAst]$paramBlockAst = $functionAst.Body.ParamBlock
+			if ($null -eq $paramBlockAst) {
+				continue
+			}
+			foreach ($parameter in $paramBlockAst.Parameters) {
+				[System.Management.Automation.Language.VariableExpressionAst]$parameterVariableAst = $parameter.Name
+				if ($parameterVariableAst.VariablePath.UserPath[0] -cmatch '[A-Z]') {
+					continue
+				}
+				## Return the diagnostic record
+				$results += [Microsoft.Windows.Powershell.ScriptAnalyzer.Generic.DiagnosticRecord]@{
+					'Message'  = "The parameter block variable '$($parameterVariableAst.VariablePath.UserPath)' needs to start with a capital letter"
+					'Extent'   = $parameterVariableAst.Extent
+					'RuleName' = $PSCmdlet.MyInvocation.InvocationName
+					'Severity' = 'Warning'
+				}
 			}
 		}
 		return $results
