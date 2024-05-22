de99be695f42883ad84adcafa332b891754817d6
neo42JBR <julian.behr@neo42.de>
2024-05-22 12:03:16 +0200

    Increase logging for failed customui

--- AppDeployToolkit/AppDeployToolkitExtensions.ps1
+++ AppDeployToolkit/AppDeployToolkitExtensions.ps1
@@ -9586,6 +9586,7 @@ function Show-NxtWelcomePrompt {
 			}
 			default
 			{
+				Write-Log "CustomAppDeployToolkitUi.ps1 returned an unknown exit code: $welcomeExitCode. Defaulting to 'Continue'..." -Severity 3 -Source ${CmdletName}
 				$returnCode = 'Continue'
 			}
 		}
