7236b846e7a931527d4b097bda2172a6077833dc
neo42JBR <149784866+neo42JBR@users.noreply.github.com>
2024-07-12 13:55:45 +0200

    961 create user account after adding password to user obj (#962)
    * move set info to later stage
    
    * add number to password for pw policy

--- test/Definitions/Add-NxtLocalUser.Tests.ps1
+++ test/Definitions/Add-NxtLocalUser.Tests.ps1
@@ -9,24 +9,24 @@ Describe 'Add-NxtLocalUser' {
             }
         }
         It 'Should create a new user account' {
-            Add-NxtLocalUser -Username 'TestUser' -Password 'JX{C@Wb<PHV=H}Gx' | Should -Be $true
+            Add-NxtLocalUser -Username 'TestUser' -Password 'JX{C@Wb<PHV=H}Gx1' | Should -Be $true
             Get-LocalUser -Name 'TestUser' | Should -Not -Be $null
         }
         It 'Should have the correct password' {
-            Add-NxtLocalUser -Username 'TestUser' -Password 'JX{C@Wb<PHV=H}Gx'
+            Add-NxtLocalUser -Username 'TestUser' -Password 'JX{C@Wb<PHV=H}Gx1'
             [System.DirectoryServices.AccountManagement.PrincipalContext]$accounts = New-Object System.DirectoryServices.AccountManagement.PrincipalContext('machine', $env:COMPUTERNAME)
-            $accounts.ValidateCredentials('TestUser', 'JX{C@Wb<PHV=H}Gx') | Should -Be $true
+            $accounts.ValidateCredentials('TestUser', 'JX{C@Wb<PHV=H}Gx1') | Should -Be $true
         }
         It 'User should have defined attributes' {
-            Add-NxtLocalUser -Username 'TestUser' -Password 'JX{C@Wb<PHV=H}Gx' -Description 'Test Description' -SetPwdNeverExpires -FullName 'Test User'
+            Add-NxtLocalUser -Username 'TestUser' -Password 'JX{C@Wb<PHV=H}Gx1' -Description 'Test Description' -SetPwdNeverExpires -FullName 'Test User'
             [Microsoft.PowerShell.Commands.LocalUser]$user = Get-LocalUser -Name 'TestUser'
             $user.FullName | Should -Be 'Test User'
             $user.Description | Should -Be 'Test Description'
             $user.PasswordExpires | Should -Be $null
         }
         It 'Should update parameters when user exists' {
-            Add-NxtLocalUser -Username 'TestUser' -Password 'JX{C@Wb<PHV=H}Gx' -Description 'Test Description' -SetPwdNeverExpires -FullName 'Test User'
-            Add-NxtLocalUser -UserName 'TestUser' -Password 'JX{C@Wb<PHV=H}Gx' -Description 'Test Description2' -SetPwdExpired -FullName 'Test User2' | Should -Be $true
+            Add-NxtLocalUser -Username 'TestUser' -Password 'JX{C@Wb<PHV=H}Gx1' -Description 'Test Description' -SetPwdNeverExpires -FullName 'Test User'
+            Add-NxtLocalUser -UserName 'TestUser' -Password 'JX{C@Wb<PHV=H}Gx1' -Description 'Test Description2' -SetPwdExpired -FullName 'Test User2' | Should -Be $true
             [Microsoft.PowerShell.Commands.LocalUser]$user = Get-LocalUser -Name 'TestUser'
             $user.FullName | Should -Be 'Test User2'
             $user.Description | Should -Be 'Test Description2'
@@ -34,7 +34,7 @@ Describe 'Add-NxtLocalUser' {
             $user.PasswordChangeableDate | Should -BeLessOrEqual (Get-Date)
         }
         It 'Should update the password of an existing user' {
-            Add-NxtLocalUser -Username 'TestUser' -Password 'JX{C@Wb<PHV=H}Gx' 
+            Add-NxtLocalUser -Username 'TestUser' -Password 'JX{C@Wb<PHV=H}Gx1' 
             Add-NxtLocalUser -UserName 'TestUser' -Password 'JX{C@Wb<PHV=H}Gx2' | Should -Be $true
             [System.DirectoryServices.AccountManagement.PrincipalContext]$accounts = New-Object System.DirectoryServices.AccountManagement.PrincipalContext('machine', $env:COMPUTERNAME)
             $accounts.ValidateCredentials('TestUser', 'JX{C@Wb<PHV=H}Gx2') | Should -Be $true
