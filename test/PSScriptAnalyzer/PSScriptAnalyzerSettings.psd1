5880fe9a3c34bd598affb980bb57ca1ca870acd8
neo42JBR <julian.behr@neo42.de>
2024-08-12 11:19:46 +0200

    add vscode settings, psscriptanalyzer and schema

---
+++ test/PSScriptAnalyzer/PSScriptAnalyzerSettings.psd1
@@ -0,0 +1,103 @@
+# PSAppDeployToolkitExtension default rules for PSScriptAnalyser, to ensure compatibility with PowerSHell 5.1 and ensure coding standards are met.
+@{
+	IncludeDefaultRules = $true
+	Severity            = @(
+		'Error',
+		'Warning',
+		'Information'
+	)
+	ExcludeRules        = @(
+		'PSUseDeclaredVarsMoreThanAssignments', # PSADT uses global variables
+		'PSAvoidGlobalVars', # PSADT uses global variables
+		'PSUseShouldProcessForStateChangingFunctions', # We don't use ShouldProcess in our scripts
+		'PSUseOutputTypeCorrectly', # Does not work good with array types
+		'PSAvoidUsingWriteHost' # Ignored because we use Write-Host in tools
+		'PSUseSingularNouns' # Ignored because we use plural nouns
+		'PSAvoidUsingEmptyCatchBlock' # Ignored because we use empty catch blocks
+	)
+	Rules               = @{
+		PSUseCompatibleCmdlets             = @{
+			Enable         = $true
+			TargetProfiles = @(
+				'desktop-5.1.14393.206-windows'
+			)
+			IgnoreCommands = @(
+			)
+		}
+		PSUseCompatibleCommands            = @{
+			Enable         = $true
+			TargetProfiles = @(
+				'win-48_x64_10.0.17763.0_5.1.17763.316_x64_4.0.30319.42000_framework'
+			)
+			IgnoreCommands = @(
+				'Should',
+				'Invoke-Pester',
+				'Write-Log'
+			)
+		}
+		PSUseCompatibleSyntax              = @{
+			Enable         = $true
+			TargetVersions = @(
+				'5.1'
+			)
+		}
+		PSUSeCompatibleTypes               = @{
+			Enable         = $true
+			TargetProfiles = @(
+				'win-48_x64_10.0.17763.0_5.1.17763.316_x64_4.0.30319.42000_framework'
+			)
+		}
+		PSProvideCommentHelp               = @{
+			Enable                  = $true
+			ExportedOnly            = $false
+			BlockComment            = $true
+			VSCodeSnippetCorrection	= $true
+			Placement               = 'begin'
+		}
+		PSUseConsistentWhitespace          = @{
+			Enable                                  = $true
+			CheckInnerBrace                         = $true
+			CheckOpenBrace                          = $true
+			CheckOpenParen                          = $true
+			CheckOperator                           = $true
+			CheckPipe                               = $true
+			CheckPipeForRedundantWhitespace         = $true
+			CheckSeparator                          = $true
+			CheckParameter                          = $true
+			IgnoreAssignmentOperatorInsideHashTable = $true
+		}
+		PSUseConsistentIndentation         = @{
+			Enable              = $true
+			PipelineIndentation = 'IncreaseIndentationForFirstPipeline'
+			Kind                = 'tab'
+		}
+		PSUseCorrectCasing                 = @{
+			Enable = $true
+		}
+		PSPlaceOpenBrace                   = @{
+			Enable             = $true
+			OnSameLine         = $true
+			NewLineAfter       = $true
+			IgnoreOneLineBlock = $false
+		}
+		PSPlaceCloseBrace                  = @{
+			Enable             = $true
+			NewLineAfter       = $true
+			IgnoreOneLineBlock = $true
+			NoEmptyLineBefore  = $true
+		}
+		PSAvoidSemicolonsAsLineTerminators = @{
+			Enable = $true
+		}
+		PSAvoidExclaimOperator             = @{
+			Enable = $true
+		}
+		PSAvoidUsingPositionalParameters   = @{
+			Enable           = $true
+			CommandAllowList = @()
+		}
+	}
+	CustomRulePath      = @(
+		'.\test\PSScriptAnalyzer\neo42PSScriptAnalyzerRules'
+	)
+}
---
+++ test/PSScriptAnalyzer/neo42PSScriptAnalyzerRules/neo42PSScriptAnalyzerRules.psm1
@@ -0,0 +1,458 @@
+function neo42PSUseCorrectTokenCapitalization {
+	<#
+	.SYNOPSIS
+	Checks that tokens are capitalized correctly.
+	.DESCRIPTION
+	Checks that tokens are capitalized correctly.
+	.INPUTS
+	[System.Management.Automation.Language.Token[]]
+	.OUTPUTS
+	[Microsoft.Windows.PowerShell.ScriptAnalyzer.Generic.DiagnosticRecord[]]
+	#>
+	[CmdletBinding()]
+	[OutputType([Microsoft.Windows.PowerShell.ScriptAnalyzer.Generic.DiagnosticRecord[]])]
+	Param (
+		[Parameter(Mandatory = $true)]
+		[ValidateNotNullOrEmpty()]
+		[System.Management.Automation.Language.Token[]]
+		$TestToken
+	)
+	Begin {
+		[string[]]$keywordList = @('if', 'else', 'elseif', 'function', 'foreach', 'for', 'while', 'do', 'in', 'switch', 'default', 'try', 'catch', 'finally', 'return', 'break', 'continue', 'throw', 'exit', 'Process', 'Begin', 'End', 'Param')
+	}
+	Process {
+		$results = @()
+		foreach ($token in $TestToken) {
+			## Check if the token is a keyword and if it is already in the correct case
+			if ($false -eq $token.TokenFlags.HasFlag([System.Management.Automation.Language.TokenFlags]::Keyword)) {
+				continue
+			}
+			## Get the correct spelling of the token
+			[string]$spelling = $keywordList | Where-Object { $_ -ieq $token.Text } | Select-Object -First 1
+			## Check if we have a suggestion, otherwise return nothing
+			if ($true -eq [string]::IsNullOrWhiteSpace($spelling) -or $spelling -ceq $token.Text) {
+				continue
+			}
+			## Create a suggestion object
+			$suggestedCorrections = New-Object System.Collections.ObjectModel.Collection["Microsoft.Windows.PowerShell.ScriptAnalyzer.Generic.CorrectionExtent"]
+			$suggestedCorrections.add(
+				[Microsoft.Windows.PowerShell.ScriptAnalyzer.Generic.CorrectionExtent]::new(
+					$token.Extent.StartLineNumber,
+					$token.Extent.EndLineNumber,
+					$token.Extent.StartColumnNumber,
+					$token.Extent.EndColumnNumber,
+					$spelling,
+					$MyInvocation.MyCommand.Definition,
+					'Apply the correct capitalization.'
+				)
+			) | Out-Null
+			## Return the diagnostic record
+			$results += [Microsoft.Windows.Powershell.ScriptAnalyzer.Generic.DiagnosticRecord]@{
+				'Message'              = 'The token is not capitalized correctly.'
+				'Extent'               = $token.Extent
+				'RuleName'             = Split-Path -Leaf $PSCmdlet.MyInvocation.InvocationName
+				'Severity'             = 'Warning'
+				'SuggestedCorrections' = $suggestedCorrections
+			}
+		}
+		return $results
+	}
+}
+
+function neo42PSVariablesInParamBlockShouldBeCapitalized {
+	<#
+	.SYNOPSIS
+	Checks that parameter variables are capitalized.
+	.DESCRIPTION
+	Checks that parameter variables are capitalized.
+	.INPUTS
+	[System.Management.Automation.Language.ScriptBlockAst]
+	.OUTPUTS
+	[Microsoft.Windows.Powershell.ScriptAnalyzer.Generic.DiagnosticRecord[]]
+	#>
+	[CmdletBinding()]
+	[OutputType([Microsoft.Windows.PowerShell.ScriptAnalyzer.Generic.DiagnosticRecord[]])]
+	Param (
+		[Parameter(Mandatory = $true)]
+		[ValidateNotNullOrEmpty()]
+		[System.Management.Automation.Language.ScriptBlockAst]
+		$TestAst
+	)
+	Process {
+		[System.Management.Automation.Language.FunctionDefinitionAst[]]$functions = $TestAst.FindAll({ $args[0] -is [System.Management.Automation.Language.FunctionDefinitionAst] }, $false)
+		$results = @()
+		foreach ($functionAst in $functions) {
+			[System.Management.Automation.Language.ParamBlockAst]$paramBlockAst = $functionAst.Body.ParamBlock
+			if ($null -eq $paramBlockAst) {
+				continue
+			}
+			foreach ($parameter in $paramBlockAst.Parameters) {
+				[System.Management.Automation.Language.VariableExpressionAst]$parameterVariableAst = $parameter.Name
+				if ($parameterVariableAst.VariablePath.UserPath[0] -cmatch '[A-Z]') {
+					continue
+				}
+				## Return the diagnostic record
+				$results += [Microsoft.Windows.Powershell.ScriptAnalyzer.Generic.DiagnosticRecord]@{
+					'Message'  = 'A parameter block variable needs to start with a capital letter'
+					'Extent'   = $parameterVariableAst.Extent
+					'RuleName' = Split-Path -Leaf $PSCmdlet.MyInvocation.InvocationName
+					'Severity' = 'Warning'
+				}
+			}
+		}
+		return $results
+	}
+}
+
+function neo42PSCapatalizedVariablesNeedToOriginateFromParamBlock {
+	<#
+	.SYNOPSIS
+	Checks that variables are capitalized and originate from the param block.
+	.DESCRIPTION
+	Checks that variables are capitalized and originate from the param block.
+	.INPUTS
+	[System.Management.Automation.Language.ScriptBlockAst]
+	.OUTPUTS
+	[Microsoft.Windows.Powershell.ScriptAnalyzer.Generic.DiagnosticRecord[]]
+	#>
+	[CmdletBinding()]
+	[OutputType([Microsoft.Windows.PowerShell.ScriptAnalyzer.Generic.DiagnosticRecord[]])]
+	Param (
+		[Parameter(Mandatory = $true)]
+		[ValidateNotNullOrEmpty()]
+		[System.Management.Automation.Language.ScriptBlockAst]
+		$TestAst
+	)
+	Process {
+		[System.Management.Automation.Language.ParamBlockAst[]]$parameterBlocks = $TestAst.FindAll({
+				$args[0] -is [System.Management.Automation.Language.ParamBlockAst] -and
+				$args[0].Parameters.Count -gt 0
+			}, $false)
+
+		$results = @()
+		foreach ($paramBlockAst in $parameterBlocks) {
+			foreach ($block in @('BeginBlock', 'ProcessBlock', 'EndBlock')) {
+				[System.Management.Automation.Language.NamedBlockAst]$namedBlockAst = $paramBlockAst.Parent | Select-Object -ExpandProperty $block -ErrorAction SilentlyContinue
+				if ($null -eq $namedBlockAst) {
+					continue
+    }
+				# Get All capitalized variables that are not automatically defined
+				[System.Management.Automation.Language.VariableExpressionAst[]]$capitalizedVariables = $namedBlockAst.FindAll({
+						$args[0] -is [System.Management.Automation.Language.VariableExpressionAst] -and
+						$args[0].VariablePath.UserPath -cmatch '^[A-Z]' -and
+						$args[0].VariablePath.UserPath -notin @('ConsoleFileName', 'EnabledExperimentalFeatures', 'Error', 'Event', 'EventArgs', 'EventSubscriber', 'ExecutionContext', 'false', 'foreach', 'HOME', 'Host', 'input', 'IsCoreCLR', 'IsLinux', 'IsMacOS', 'IsWindows', 'LASTEXITCODE', 'Matches', 'MyInvocation', 'HostInvocation', 'NestedPromptLevel', 'null', 'PID', 'PROFILE', 'PSBoundParameters', 'PSCmdlet', 'PSCommandPath', 'PSCulture', 'PSDebugContext', 'PSEdition', 'PSHOME', 'PSItem', 'PSScriptRoot', 'PSSenderInfo', 'PSUICulture', 'PSVersionTable', 'PWD', 'Sender', 'ShellId', 'StackTrace', 'switch', 'this', 'true')
+					}, $false)
+
+				foreach ($variableAst in $capitalizedVariables) {
+					if ($variableAst.VariablePath.UserPath -in $paramBlockAst.Parameters.Name.VariablePath.UserPath) {
+						continue
+					}
+					$results += [Microsoft.Windows.Powershell.ScriptAnalyzer.Generic.DiagnosticRecord]@{
+						'Message'  = 'A capatalized variable needs to be defined in the param block'
+						'Extent'   = $variableAst.Extent
+						'RuleName' = Split-Path -Leaf $PSCmdlet.MyInvocation.InvocationName
+						'Severity' = 'Warning'
+					}
+				}
+			}
+		}
+		return $results
+	}
+}
+
+function neo42PSParamBlockVariablesShouldBeTyped {
+	<#
+	.SYNOPSIS
+	Checks that parameter variables are typed.
+	.DESCRIPTION
+	Checks that parameter variables are typed.
+	.INPUTS
+	[System.Management.Automation.Language.ScriptBlockAst]
+	.OUTPUTS
+	[Microsoft.Windows.Powershell.ScriptAnalyzer.Generic.DiagnosticRecord[]]
+	#>
+	[CmdletBinding()]
+	[OutputType([Microsoft.Windows.PowerShell.ScriptAnalyzer.Generic.DiagnosticRecord[]])]
+	Param (
+		[Parameter(Mandatory = $true)]
+		[ValidateNotNullOrEmpty()]
+		[System.Management.Automation.Language.ScriptBlockAst]
+		$TestAst
+	)
+	Process {
+		[System.Management.Automation.Language.ParamBlockAst]$parameterBlock = $TestAst.ParamBlock
+		if ($null -eq $parameterBlock) {
+			return
+		}
+
+		$results = @()
+		foreach ($parameterAst in $parameterBlock.Parameters) {
+			if ($null -eq $parameterAst.Attributes.TypeName) {
+				$results += [Microsoft.Windows.Powershell.ScriptAnalyzer.Generic.DiagnosticRecord]@{
+					'Message'  = 'A parameter block variable needs to be typed'
+					'Extent'   = $parameterAst.Extent
+					'RuleName' = Split-Path -Leaf $PSCmdlet.MyInvocation.InvocationName
+					'Severity' = 'Warning'
+				}
+			}
+			elseif ($parameterAst.Attributes.TypeName.Extent.StartLineNumber -eq $parameterAst.Name.Extent.StartLineNumber) {
+				$results += [Microsoft.Windows.Powershell.ScriptAnalyzer.Generic.DiagnosticRecord]@{
+					'Message'  = 'The type definition and variable should be on a seperate lines'
+					'Extent'   = $parameterAst.Extent
+					'RuleName' = Split-Path -Leaf $PSCmdlet.MyInvocation.InvocationName
+					'Severity' = 'Warning'
+				}
+			}
+		}
+		return $results
+	}
+}
+
+function neo42PSDontUseEmptyStringLiterals {
+	<#
+	.SYNOPSIS
+	Checks that empty strings are not used.
+	.DESCRIPTION
+	Checks that empty strings are not used.
+	.INPUTS
+	[System.Management.Automation.Language.ScriptBlockAst]
+	.OUTPUTS
+	[Microsoft.Windows.Powershell.ScriptAnalyzer.Generic.DiagnosticRecord[]]
+	#>
+	[CmdletBinding()]
+	[OutputType([Microsoft.Windows.PowerShell.ScriptAnalyzer.Generic.DiagnosticRecord[]])]
+	Param (
+		[Parameter(Mandatory = $true)]
+		[ValidateNotNullOrEmpty()]
+		[System.Management.Automation.Language.ScriptBlockAst]
+		$TestAst
+	)
+	Process {
+		[System.Management.Automation.Language.StringConstantExpressionAst[]]$stringConstants = $TestAst.FindAll({
+				$args[0] -is [System.Management.Automation.Language.StringConstantExpressionAst] -and $args[0].Value -eq [string]::Empty -and
+				$args[0].Parent.TypeName.Name -ne 'Diagnostics.CodeAnalysis.SuppressMessageAttribute'
+			}, $false)
+		$results = @()
+		foreach ($stringConstant in $stringConstants) {
+			$suggestedCorrections = New-Object System.Collections.ObjectModel.Collection["Microsoft.Windows.PowerShell.ScriptAnalyzer.Generic.CorrectionExtent"]
+			$suggestedCorrections.add(
+				[Microsoft.Windows.PowerShell.ScriptAnalyzer.Generic.CorrectionExtent]::new(
+					$stringConstant.Extent.StartLineNumber,
+					$stringConstant.Extent.EndLineNumber,
+					$stringConstant.Extent.StartColumnNumber,
+					$stringConstant.Extent.EndColumnNumber,
+					'[string]::Empty',
+					$MyInvocation.MyCommand.Definition,
+					'Use .NET string empty instead of empty string literal.'
+				)
+			) | Out-Null
+			$results += [Microsoft.Windows.Powershell.ScriptAnalyzer.Generic.DiagnosticRecord]@{
+				'Message'              = 'Empty strings should not be used'
+				'Extent'               = $stringConstant.Extent
+				'RuleName'             = Split-Path -Leaf $PSCmdlet.MyInvocation.InvocationName
+				'Severity'             = 'Warning'
+				'SuggestedCorrections' = $suggestedCorrections
+			}
+		}
+		return $results
+	}
+}
+
+function neo42PSEnforceConsistantConditionalStatements {
+	<#
+	.SYNOPSIS
+	Checks that conditional statements are consistent.
+	.DESCRIPTION
+	Checks that conditional statements are consistent.
+	.INPUTS
+	[System.Management.Automation.Language.ScriptBlockAst]
+	.OUTPUTS
+	[Microsoft.Windows.Powershell.ScriptAnalyzer.Generic.DiagnosticRecord[]]
+	#>
+	[CmdletBinding()]
+	[OutputType([Microsoft.Windows.PowerShell.ScriptAnalyzer.Generic.DiagnosticRecord[]])]
+	Param (
+		[Parameter(Mandatory = $true)]
+		[ValidateNotNullOrEmpty()]
+		[System.Management.Automation.Language.ScriptBlockAst]
+		$TestAst
+	)
+	$results = @()
+	[System.Management.Automation.Language.BinaryExpressionAst[]]$wrongSideOperators = $TestAst.FindAll({
+			$args[0] -is [System.Management.Automation.Language.BinaryExpressionAst] -and
+			$args[0].Right.Extent.Text -in @('$true', '$false')
+		}, $false)
+	foreach ($wrongSideOperator in $wrongSideOperators) {
+		$suggestedCorrections = New-Object System.Collections.ObjectModel.Collection["Microsoft.Windows.PowerShell.ScriptAnalyzer.Generic.CorrectionExtent"]
+		$suggestedCorrections.add(
+			[Microsoft.Windows.PowerShell.ScriptAnalyzer.Generic.CorrectionExtent]::new(
+				$wrongSideOperator.Extent.StartLineNumber,
+				$wrongSideOperator.Extent.EndLineNumber,
+				$wrongSideOperator.Extent.StartColumnNumber,
+				$wrongSideOperator.Extent.EndColumnNumber,
+				$wrongSideOperator.Right.Extent.Text + ' -' + $wrongSideOperator.Operator + ' ' + $wrongSideOperator.Left.Extent.Text,
+				$MyInvocation.MyCommand.Definition,
+				'Switch the boolean literal to the left side of the comparison.'
+			)
+		) | Out-Null
+		$results += [Microsoft.Windows.Powershell.ScriptAnalyzer.Generic.DiagnosticRecord]@{
+			'Message'              = 'Boolean literals should be on the left side of a comparison'
+			'Extent'               = $wrongSideOperator.Extent
+			'RuleName'             = Split-Path -Leaf $PSCmdlet.MyInvocation.InvocationName
+			'Severity'             = 'Warning'
+			'SuggestedCorrections' = $suggestedCorrections
+		}
+	}
+
+	<# Currently very basic... Can only detect conditions without any operator
+	$noOperators = $TestAst.FindAll({
+			(
+				$args[0] -is [System.Management.Automation.Language.IfStatementAst] -and
+				$args[0].Clauses.Item1.Extent.Text -notmatch ('-')
+			) -or
+			(
+				(
+					$args[0] -is [System.Management.Automation.Language.DoUntilStatementAst] -or
+					$args[0] -is [System.Management.Automation.Language.DoWhileStatementAst] -or
+					$args[0] -is [System.Management.Automation.Language.WhileStatementAst]
+				) -and
+				$args[0].Condition.Extent.Text -notmatch ('-')
+			)
+		}, $false)
+	foreach ($noOperator in $noOperators) {
+		$suggestedCorrections = New-Object System.Collections.ObjectModel.Collection["Microsoft.Windows.PowerShell.ScriptAnalyzer.Generic.CorrectionExtent"]
+		$suggestedCorrections.add(
+			[Microsoft.Windows.PowerShell.ScriptAnalyzer.Generic.CorrectionExtent]::new(
+				$noOperator.Extent.StartLineNumber,
+				$noOperator.Extent.EndLineNumber,
+				$noOperator.Extent.StartColumnNumber,
+				$noOperator.Extent.EndColumnNumber,
+				'$true -eq ' + $noOperator.Extent.Text,
+				$MyInvocation.MyCommand.Definition,
+				'Evaluate the condition with an operator against a boolean literal.'
+			)
+		) | Out-Null
+		$results += [Microsoft.Windows.Powershell.ScriptAnalyzer.Generic.DiagnosticRecord]@{
+			'Message'  = 'Conditional statements should be used in conjunction with an operator'
+			'Extent'   = $noOperator.Extent
+			'RuleName' = Split-Path -Leaf $PSCmdlet.MyInvocation.InvocationName
+			'Severity' = 'Warning'
+		}
+	}
+	#>
+
+	return $results
+}
+
+function neo42PSEnforceNewLineAtEndOfFile {
+	<#
+	.SYNOPSIS
+	Checks that there is a new line at the end of the file.
+	.DESCRIPTION
+	Checks that there is a new line at the end of the file.
+	.INPUTS
+	[System.Management.Automation.Language.ScriptBlockAst]
+	.OUTPUTS
+	[Microsoft.Windows.Powershell.ScriptAnalyzer.Generic.DiagnosticRecord[]]
+	#>
+	[CmdletBinding()]
+	[OutputType([Microsoft.Windows.PowerShell.ScriptAnalyzer.Generic.DiagnosticRecord[]])]
+	Param (
+		[Parameter(Mandatory = $true)]
+		[ValidateNotNullOrEmpty()]
+		[System.Management.Automation.Language.ScriptBlockAst]
+		$TestAst
+	)
+	Process {
+		if ($null -ne $TestAst.Parent) {
+			return
+		}
+		[string]$lastLine = $TestAst.Extent.Text -split [System.Environment]::NewLine | Select-Object -Last 1
+		if ($true -eq [string]::IsNullOrWhiteSpace($lastLine)) {
+			return
+		}
+		$suggestedCorrections = New-Object System.Collections.ObjectModel.Collection["Microsoft.Windows.PowerShell.ScriptAnalyzer.Generic.CorrectionExtent"]
+		$suggestedCorrections.add(
+			[Microsoft.Windows.PowerShell.ScriptAnalyzer.Generic.CorrectionExtent]::new(
+				$TestAst.Extent.EndLineNumber,
+				$TestAst.Extent.EndLineNumber,
+				1,
+				$TestAst.Extent.EndColumnNumber,
+				$lastLine + "`r`n",
+				$MyInvocation.MyCommand.Definition,
+				'Add a new line at the end of the file.'
+			)
+		) | Out-Null
+		$extent = [System.Management.Automation.Language.ScriptExtent]::new(
+			[System.Management.Automation.Language.ScriptPosition]::new(
+				$TestAst.Extent.File,
+				$TestAst.Extent.EndLineNumber,
+				1,
+				$lastLine
+			),
+			[System.Management.Automation.Language.ScriptPosition]::new(
+				$TestAst.Extent.File,
+				$TestAst.Extent.EndLineNumber,
+				$lastLine.Length,
+				$lastLine
+			)
+		)
+		return [Microsoft.Windows.Powershell.ScriptAnalyzer.Generic.DiagnosticRecord]@{
+			'Message'              = 'There should be a new line at the end of the file'
+			'Extent'               = $extent
+			'RuleName'             = Split-Path -Leaf $PSCmdlet.MyInvocation.InvocationName
+			'Severity'             = 'Warning'
+			'SuggestedCorrections' = $suggestedCorrections
+		}
+	}
+}
+
+function neo42PSIncompatibleFunctions {
+	<#
+	.SYNOPSIS
+	Dont allow usage of PSADT functions which are not compatible with our extensions.
+	.DESCRIPTION
+	Dont allow usage of PSADT functions which are not compatible with our extensions.
+	.INPUTS
+	[System.Management.Automation.Language.ScriptBlockAst]
+	.OUTPUTS
+	[Microsoft.Windows.Powershell.ScriptAnalyzer.Generic.DiagnosticRecord[]]
+	#>
+	[CmdletBinding()]
+	[OutputType([Microsoft.Windows.PowerShell.ScriptAnalyzer.Generic.DiagnosticRecord[]])]
+	Param (
+		[Parameter(Mandatory = $true)]
+		[ValidateNotNullOrEmpty()]
+		[System.Management.Automation.Language.ScriptBlockAst]
+		$TestAst
+	)
+	Begin {
+		[psobject[]]$incompatibleFunctions = @(
+			@{
+				Functions = @('Update-SessionEnvironmentVariables', 'Refresh-SessionEnvironmentVariables')
+				Reason    = 'Due to security reasons we clear the environment at the start of the Deploy-Application.ps1. Reloading the environment would mitigate this security measure.'
+			}
+		)
+	}
+	Process {
+		[System.Management.Automation.Language.CommandAst[]]$commandAsts = $TestAst.FindAll({
+				$args[0] -is [System.Management.Automation.Language.CommandAst] -and
+				$args[0].GetCommandName() -in @($incompatibleFunctions.Functions)
+			}, $false)
+
+		[Microsoft.Windows.PowerShell.ScriptAnalyzer.Generic.DiagnosticRecord[]]$results = @()
+
+		foreach ($commandAst in $commandAsts) {
+			$results += [Microsoft.Windows.Powershell.ScriptAnalyzer.Generic.DiagnosticRecord]@{
+				'Message'              = ($incompatibleFunctions | Where-Object { $_.Functions -contains $commandAst.GetCommandName() }).Reason
+				'Extent'               = $commandAst.Extent
+				'RuleName'             = Split-Path -Leaf $PSCmdlet.MyInvocation.InvocationName
+				'Severity'             = 'Warning'
+			}
+		}
+
+		return $results
+	}
+}
+
+Export-ModuleMember -Function 'neo42*'
