65fd4eb7519edbb12e2c760da5ea11752e14181e
neo42JBR <julian.behr@neo42.de>
2024-08-26 13:47:20 +0200

    remove broken tests

--- test/Definitions/CheckCodingGuidelines.Tests.ps1
+++ test/Definitions/CheckCodingGuidelines.Tests.ps1
@@ -75,7 +75,7 @@ Describe "Coding Guidelines" -ForEach @(
         }
         It "Capitalized variables should be defined in the param block" -Skip {
             $parameterBlocks = $ast.FindAll({
-                param($ast) 
+                param($ast)
                 $ast -is [System.Management.Automation.Language.ParamBlockAst] -and
                 $ast.Parameters.Name.VariablePath.UserPath.Count -gt 0
             }, $true)
@@ -94,7 +94,7 @@ Describe "Coding Guidelines" -ForEach @(
                     $capitalizedVariables | ForEach-Object {
                         $_ | Should -BeIn $paramBlockAst.Parameters.Name.VariablePath.UserPath -Because "the capitalized variable '$($_.VariablePath.UserPath)' is not defined in the param block (line $($_.Extent.StartLineNumber))"
                     }
-                    
+
                 }
             }
         }
@@ -103,18 +103,18 @@ Describe "Coding Guidelines" -ForEach @(
                 Set-ItResult -Skipped -Because "the file '$($path)' is excluded from this test"
                 return
             }
-            [System.Management.Automation.Language.FunctionDefinitionAst[]]$extensionFunctions = $ast.FindAll({ 
-                $args[0] -is [System.Management.Automation.Language.FunctionDefinitionAst] 
+            [System.Management.Automation.Language.FunctionDefinitionAst[]]$extensionFunctions = $ast.FindAll({
+                $args[0] -is [System.Management.Automation.Language.FunctionDefinitionAst]
             }, $true)
-            [System.Management.Automation.Language.CommandAst[]]$commandCallsAsts = $ast.FindAll({ 
+            [System.Management.Automation.Language.CommandAst[]]$commandCallsAsts = $ast.FindAll({
                 param($ast)
-                $ast -is [System.Management.Automation.Language.CommandAst] -and 
+                $ast -is [System.Management.Automation.Language.CommandAst] -and
                 $ast.GetCommandName() -in $extensionFunctions.Name -and
                 $ast.GetCommandName() -notin @("Write-Log", "Exit-NxtScriptWithError", "Set-NxtPackageArchitecture", "Expand-NxtPackageConfig", "Show-NxtWelcomePrompt")
             }, $true)
             foreach ($commandAst in $commandCallsAsts) {
                 [System.Management.Automation.Language.FunctionDefinitionAst]$calledFunction = $extensionFunctions | Where-Object {$_.Name -eq $commandAst.GetCommandName()}
-                [System.Management.Automation.Language.ParameterAst[]]$calledFunctionRequiredParameters = $calledFunction.Body.ParamBlock.Parameters | Where-Object { 
+                [System.Management.Automation.Language.ParameterAst[]]$calledFunctionRequiredParameters = $calledFunction.Body.ParamBlock.Parameters | Where-Object {
                     $null -ne $_.DefaultValue -and
                     $_.DefaultValue.ToString().Contains("PackageConfig") -and
                     $_.Name.VariablePath.UserPath -notin @('DisableLogging')
@@ -234,31 +234,6 @@ Describe "Coding Guidelines" -ForEach @(
                     $ast.Right.Extent.Text -in @('$true', '$false', '$null')
                 }, $true)
             $wrongSideOperator | Should -BeNullOrEmpty -Because "there should be no null or empty on the right side of a comparison (line $($wrongSideOperator.Extent.StartLineNumber))"
-
-            $unaryExpression = $ast.Find({
-                    param($ast)
-                    $ast -is [System.Management.Automation.Language.UnaryExpressionAst] -and
-                    $ast.Extent.Text -notmatch "\$\w+\+\+" -and
-                    $ast.Extent.Text -notmatch "\$\w+\-\-"
-                }, $true)
-            $unaryExpression | Should -BeNullOrEmpty -Because "there should be no unary expressions (line $($unaryExpression.Extent.StartLineNumber))"
-
-            $noOperator = $ast.Find({
-                param($ast)
-                (
-                    $ast -is [System.Management.Automation.Language.IfStatementAst] -and
-                    $ast.Clauses.Item1.Extent.Text -notmatch ("-")
-                ) -or
-                (
-                    (
-                        $ast -is [System.Management.Automation.Language.DoUntilStatementAst] -or
-                        $ast -is [System.Management.Automation.Language.DoWhileStatementAst] -or
-                        $ast -is [System.Management.Automation.Language.WhileStatementAst]
-                    ) -and
-                    $ast.Condition.Extent.Text -notmatch ("-")
-                )
-            }, $true)
-            $noOperator | Should -BeNullOrEmpty -Because "there should be no condition without and operator (line $($noOperator.Extent.StartLineNumber))"
         }
         It "Check if command aliases have been used" {
             $commands = $ast.FindAll({
