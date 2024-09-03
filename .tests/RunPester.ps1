1463359d873548f4016d32eb6a0789c48ae85070
neo42JBR <julian.behr@neo42.de>
2024-09-03 11:51:41 +0200

    Allow setting path to main

--- .tests/RunPester.ps1
+++ .tests/RunPester.ps1
@@ -1,16 +1,23 @@
 <#
-    .SYNOPSIS
-        Starts pester with our configuration
-    .NOTES
-        # LICENSE #
-        This program is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser General Public License as published by the Free Software Foundation, either version 3 of the License, or any later version. This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
-        You should have received a copy of the GNU Lesser General Public License along with this program. If not, see <http://www.gnu.org/licenses/>.
+	.SYNOPSIS
+		Starts pester with our configuration
+	.NOTES
+		# LICENSE #
+		This program is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser General Public License as published by the Free Software Foundation, either version 3 of the License, or any later version. This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+		You should have received a copy of the GNU Lesser General Public License along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-        # COPYRIGHT #
-        Copyright (c) 2024 neo42 GmbH, Germany.
+		# COPYRIGHT #
+		Copyright (c) 2024 neo42 GmbH, Germany.
 #>
-
 #requires -module Pester -version 5
+
+[CmdletBinding()]
+[Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSAvoidUsingWriteHost', '')]
+Param (
+	[System.IO.FileInfo]
+	$ToolkitMain = (Get-ChildItem -Recurse -Filter 'AppDeployToolkitMain.ps1' | Select-Object -First 1)
+)
+
 Import-Module Pester
 
 # Pester config
@@ -30,14 +37,13 @@ $compilerArgs = "/target:winexe /out:$global:simpleExe $PSScriptRoot\simple.cs"
 Start-Process -FilePath $compilerPath -ArgumentList $compilerArgs -Wait -NoNewWindow
 
 # Mute Toolkit logging
-[System.IO.FileInfo]$toolkitMain = Get-ChildItem -Recurse -Filter "AppDeployToolkitMain.ps1" | Select-Object -First 1
-(Get-Content "$($toolkitMain.Directory.FullName)\AppDeployToolkitConfig.xml" -Raw).Replace('<Toolkit_LogWriteToHost>true</Toolkit_LogWriteToHost>', '<Toolkit_LogWriteToHost>false</Toolkit_LogWriteToHost>') |
-    Out-File "$($toolkitMain.Directory.FullName)\AppDeployToolkitConfig.xml"
+(Get-Content "$($ToolkitMain.Directory.FullName)\AppDeployToolkitConfig.xml" -Raw).Replace('<Toolkit_LogWriteToHost>true</Toolkit_LogWriteToHost>', '<Toolkit_LogWriteToHost>false</Toolkit_LogWriteToHost>') |
+	Out-File "$($ToolkitMain.Directory.FullName)\AppDeployToolkitConfig.xml"
 
 # Import PSADT
-[string]$global:PSADTPath = $toolkitMain.Directory.Parent.FullName
-Write-Host "Importing AppDeployToolkitMain.ps1 from [$($toolkitMain.FullName)]"
-. $toolkitMain.FullName -DisableLogging
+[string]$global:PSADTPath = $ToolkitMain.Directory.Parent.FullName
+Write-Host "Importing AppDeployToolkitMain.ps1 from [$($ToolkitMain.FullName)]"
+. $ToolkitMain.FullName -DisableLogging
 
 # Run Pester
 Invoke-Pester -Configuration $config
