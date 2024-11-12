e1c61e4ecf0087dd9dba800db166d2becb48f822
RKH <Rene.Knauth@neo42.de>
2024-11-12 16:04:43 +0100

    change variable name inside loop

--- AppDeployToolkit/AppDeployToolkitExtensions.ps1
+++ AppDeployToolkit/AppDeployToolkitExtensions.ps1
@@ -12414,8 +12414,8 @@ function Unregister-NxtOld {
 				Write-Log -Message "DeploymentSystem is set to 'Empirum', skipping unregister of Empirum packages in 'HKLM:\Software'." -Source ${cmdletName}
 				continue
 			}
-			foreach ($appName in $appNameList) {
-				[Microsoft.Win32.RegistryKey]$regProductKey = Get-Item -Path "$regPackageRoot\$RegPackagesKey\$AppVendor\$appName" -ErrorAction SilentlyContinue
+			foreach ($appNameItem in $appNameList) {
+				[Microsoft.Win32.RegistryKey]$regProductKey = Get-Item -Path "$regPackageRoot\$RegPackagesKey\$AppVendor\$appNameItem" -ErrorAction SilentlyContinue
 				if ($null -eq $regProductKey) {
 					continue
 				}
@@ -12428,25 +12428,25 @@ function Unregister-NxtOld {
 				[Microsoft.Win32.RegistryKey[]]$regVersionKeysOfNonADTPackages = $regVersionKeys | Where-Object {
 					$true -eq [string]::IsNullOrEmpty($_.GetValue("PackageGUID"))
 				}
-				Write-Log -Message "Detected $($regVersionKeysOfNonADTPackages.Count) traditional Empirum installation(s) of '$appName'." -Source ${cmdletName}
+				Write-Log -Message "Detected $($regVersionKeysOfNonADTPackages.Count) traditional Empirum installation(s) of '$appNameItem'." -Source ${cmdletName}
 				foreach ($regVersionKey in $regVersionKeysOfNonADTPackages) {
 					[Microsoft.Win32.RegistryKey]$regSetupKey = Get-Item -Path (Join-Path $regVersionKey.PSPath "Setup")
 					## Remove this entry if the setup information is not available
 					if (($null -eq $regSetupKey) -or ($true -eq [string]::IsNullOrEmpty($regSetupKey.GetValue("Version")))) {
-						Write-Log "The setup information for the package '$appName' could not be found. Removing old entry." -Source ${CmdletName} -Severity 2
+						Write-Log "The setup information for the package '$appNameItem' could not be found. Removing old entry." -Source ${CmdletName} -Severity 2
 						Remove-Item -Path $regVersionKey.PSPath -Recurse
 						Remove-NxtEmptyRegistryKey -Path (Split-Path -Parent -Path $regVersionKey.Name)
 						continue
 					}
 					[string]$packageVersion = $regSetupKey.GetValue("Version")
 					## Obtain the uninstall key for the package
-					[Microsoft.Win32.RegistryKey]$regUninstallKey = Get-Item -Path "$regPackageRoot\Microsoft\Windows\CurrentVersion\Uninstall\neoPackage $AppVendor $appName $packageVersion" -ErrorAction SilentlyContinue
+					[Microsoft.Win32.RegistryKey]$regUninstallKey = Get-Item -Path "$regPackageRoot\Microsoft\Windows\CurrentVersion\Uninstall\neoPackage $AppVendor $appNameItem $packageVersion" -ErrorAction SilentlyContinue
 					if ($null -ne $regUninstallKey) {
-						Write-Log -Message "Removing the uninstall key for the package '$appName' with version '$packageVersion'." -Source ${CmdletName}
+						Write-Log -Message "Removing the uninstall key for the package '$appNameItem' with version '$packageVersion'." -Source ${CmdletName}
 						Remove-Item -Path $regUninstallKey.PSPath
 					}
 					else {
-						Write-Log -Message "The uninstall key for the package '$appName' with version '$packageVersion' could not be found." -Source ${CmdletName} -Severity 2
+						Write-Log -Message "The uninstall key for the package '$appNameItem' with version '$packageVersion' could not be found." -Source ${CmdletName} -Severity 2
 					}
 					Remove-Item -Path $regVersionKey.PSPath -Recurse
 					Remove-NxtEmptyRegistryKey -Path $regProductKey.Name
