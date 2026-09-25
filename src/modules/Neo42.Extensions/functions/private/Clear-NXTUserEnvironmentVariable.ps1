7e3791593249ee43022a5af3f247b868ebc17dde
neo42JBR <julian.behr@neo42.de>
2026-09-25 17:22:01 +0200

    Change function name

---
+++ src/modules/Neo42.Extensions/functions/private/Clear-NXTUserEnvironmentVariable.ps1
@@ -0,0 +1,25 @@
+﻿function Clear-NXTUserEnvironmentVariable {
+	<#
+	.SYNOPSIS
+	Clears all user defined environment variables from the process environment.
+	#>
+	[System.Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseShouldProcessForStateChangingFunctions', '', Justification = 'This is a private function')]
+	[CmdletBinding()]
+	param()
+	begin {
+		Initialize-ADTFunction -Cmdlet $PSCmdlet -SessionState $ExecutionContext.SessionState
+	}
+	process {
+		Write-ADTLogEntry -Message 'Removing user environment variables from the process environment.' -DebugMessage
+		foreach ($name in [System.Environment]::GetEnvironmentVariables([System.EnvironmentVariableTarget]::User).Keys) {
+			[System.Environment]::SetEnvironmentVariable(
+				$name,
+				[System.Environment]::GetEnvironmentVariable($name, [System.EnvironmentVariableTarget]::Machine),
+				[System.EnvironmentVariableTarget]::Process
+			)
+		}
+	}
+	end {
+		Complete-ADTFunction -Cmdlet $PSCmdlet
+	}
+}
